120c7679c7681aa553e8fb4c979484234760d2fc
Poveii <yellowtutors@gmail.com>
2025-05-08 06:55:50 -0300

    build: Add Nunito Sans fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from '@expo-google-fonts/nunito-sans';
 import { StatusBar } from 'expo-status-bar';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 
 import theme from '@/theme';
 import { Routes } from '@/routes';
 
+import { Loading } from '@/components/Loading';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      {fontsLoaded ? <Routes /> : <Loading />}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
